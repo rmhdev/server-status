ccd5e1a15961a303854ace33d5e71fe1da0fa3f7
Rober MH <rmh.dev@gmail.com>
2018-02-03 16:59:39 +0100

    preparing measurement summaries

---
+++ src/Application/DataTransformer/Measurement/MeasurementSummaryDtoDataTransformer.php
@@ -0,0 +1,26 @@
+<?php
+declare(strict_types=1);
+
+/**
+ * This file is part of the server-status package.
+ *
+ * (c) Roberto Martin <rmh.dev@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace ServerStatus\Application\DataTransformer\Measurement;
+
+final class MeasurementSummaryDtoDataTransformer implements MeasurementSummaryDataTransformer
+{
+    public function write($measurements = [])
+    {
+        // TODO: Implement write() method.
+    }
+
+    public function read()
+    {
+        // TODO: Implement read() method.
+    }
+}
