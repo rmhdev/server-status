055a8ed8e85d271c661cf350c4e2cc9f17055f3e
Rober MH <rmh.dev@gmail.com>
2018-02-20 11:36:43 +0100

    calculate averages in last 24 hours summary

--- src/ServerStatus/Domain/Model/Measurement/Summary/MeasureLast24HoursSummary.php
+++ src/ServerStatus/Domain/Model/Measurement/Summary/MeasureLast24HoursSummary.php
@@ -24,6 +24,11 @@ class MeasureLast24HoursSummary
         $this->date = $date->format(DATE_ISO8601);
     }
 
+    protected function values(): array
+    {
+        return $this->values;
+    }
+
     protected function date(): \DateTimeImmutable
     {
         return \DateTimeImmutable::createFromFormat(DATE_ISO8601, $this->date);
@@ -54,9 +59,47 @@ class MeasureLast24HoursSummary
         return $averages;
     }
 
-    private function average(\DateTimeInterface $date)
+    /**
+     * @inheritdoc
+     */
+    public function average(\DateTimeInterface $fromDate): SummaryAverage
     {
-        return [];
+        list($start, $end) = $this->startEndDates($fromDate);
+
+        return new SummaryAverage(
+            $start,
+            $end,
+            $this->filterValues($start, $end)
+        );
+    }
+
+    /**
+     * Calculate the group start and end dates.
+     *
+     * @param \DateTimeInterface $fromDate
+     * @return \DateTimeImmutable[]
+     */
+    protected function startEndDates(\DateTimeInterface $fromDate): array
+    {
+        $date = \DateTimeImmutable::createFromFormat(DATE_ISO8601, $fromDate->format(DATE_ISO8601));
+        $formattedMinute = (int) $date->format("i");
+        $formattedMinute -= $formattedMinute % $this->groupedByMinutes();
+        $start = $date->setTime((int) $date->format("H"), $formattedMinute, 0);
+        $end = $start->modify(sprintf("+%d minutes - 1 second", $this->groupedByMinutes()));
+
+        return [$start, $end];
+    }
+
+    protected function filterValues(\DateTimeImmutable $from, \DateTimeImmutable $to): array
+    {
+        return array_filter($this->values(), function ($value) use ($from, $to) {
+            if (!array_key_exists("date", $value)) {
+                return false;
+            }
+            $date = new \DateTimeImmutable($value["date"]);
+
+            return $date >= $from && $date <= $to;
+        });
     }
 
     /**
--- tests/ServerStatus/Domain/Model/Measurement/Summary/MeasureLast24HoursSummaryTest.php
+++ tests/ServerStatus/Domain/Model/Measurement/Summary/MeasureLast24HoursSummaryTest.php
@@ -81,4 +81,52 @@ class MeasureLast2HoursSummaryTest extends TestCase
 
         $this->assertEquals(24 * (60 / 10), sizeof($averages));
     }
+
+    /**
+     * @test
+     */
+    public function itShouldCalculateCorrectAverages()
+    {
+        $rawValues = [
+            // should ignore previous values: ..19
+            [
+                "date" => "2018-02-03T15:19:00+0200",
+                "response_time" => 1.000400,
+                "count" => 1
+            ],
+            // testing values: minutes 20..29
+            [
+                "date" => "2018-02-03T15:20:00+0200",
+                "response_time" => 1.000100,
+                "count" => 1
+            ],
+            [
+                "date" => "2018-02-03T15:21:00+0200",
+                "response_time" => 1.000200,
+                "count" => 1
+            ],
+            [
+                "date" => "2018-02-03T15:22:00+0200",
+                "response_time" => 1.000300,
+                "count" => 1
+            ],
+            [
+                "date" => "2018-02-03T15:23:00+0200",
+                "response_time" => 1.000400,
+                "count" => 1
+            ],
+            // should ignore next values: 30..
+            [
+                "date" => "2018-02-03T15:30:00+0200",
+                "response_time" => 1.000400,
+                "count" => 1
+            ],
+        ];
+        $summary = new MeasureLast24HoursSummary($rawValues, new \DateTime("2018-02-03T15:24:10+0200"));
+        $average = $summary->average(new \DateTime("2018-02-03T15:23:00+0200"));
+
+        $this->assertEquals(new \DateTimeImmutable("2018-02-03T15:20:00+0200"), $average->from());
+        $this->assertEquals(new \DateTimeImmutable("2018-02-03T15:29:59+0200"), $average->to());
+        $this->assertEquals(1.000250, $average->responseTime());
+    }
 }
