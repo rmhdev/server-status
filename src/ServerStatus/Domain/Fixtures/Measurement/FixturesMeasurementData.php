643166901987d123d2b9f6f50a488660f1dd6cc5
Rober MH <rmh.dev@gmail.com>
2018-02-25 17:42:31 +0100

    improving fixtures data

--- src/ServerStatus/Domain/Fixtures/Measurement/FixturesMeasurementData.php
+++ src/ServerStatus/Domain/Fixtures/Measurement/FixturesMeasurementData.php
@@ -23,10 +23,12 @@ use ServerStatus\Domain\Model\Measurement\MeasurementResult;
 
 final class FixturesMeasurementData
 {
-    const MINUTES_BETWEEN_MEASUREMENTS = 5;
+    const MINUTES_BETWEEN_MEASUREMENTS_CONDENSED = 10;
+    const MINUTES_BETWEEN_MEASUREMENTS = 60;
     const MINUTES_PER_DAY = 1440;
     const AVAILABLE_MONTHS = 1;
     const START_DATE = "2018-01-01T00:00:00+0200";
+    const NUM_WEEK_IN_YEAR_CONDENSED = 1;
 
     /**
      * @var MeasurementRepository
@@ -87,29 +89,67 @@ final class FixturesMeasurementData
      */
     private function createMeasurementsForDate(Check $check, \DateTimeInterface $dateTime): array
     {
+        /* @var \DateTimeImmutable $start */
+        $weekNumber = $dateTime->format("W");
         $start = \DateTimeImmutable::createFromFormat(DATE_ISO8601, $dateTime->format(DATE_ISO8601))
             ->setTime(0, 0, 0)
             ->getTimestamp();
         $measurements = [];
-        for ($i = 0; $i < self::MINUTES_PER_DAY; $i += self::MINUTES_BETWEEN_MEASUREMENTS) {
+
+        /**
+         * To minimize db size:
+         * We will add more measurements only for a given week of the year;
+         * the rest of dates will have less measurements.
+         */
+        $addMinutes = self::NUM_WEEK_IN_YEAR_CONDENSED == $weekNumber ?
+            self::MINUTES_BETWEEN_MEASUREMENTS_CONDENSED :
+            self::MINUTES_BETWEEN_MEASUREMENTS;
+
+        for ($i = 0; $i < self::MINUTES_PER_DAY; $i += $addMinutes) {
+            $dateCreated = \DateTime::createFromFormat(DATE_ISO8601, date(DATE_ISO8601, $start + ($i * 60)));
             $measurements[] = $this->factory->build(
-                new MeasurementId($check->id()->id() . "-" . $dateTime->format("Y-m-d") . "-" . $i),
-                \DateTime::createFromFormat(DATE_ISO8601, date(DATE_ISO8601, $start + ($i * 60))),
+                new MeasurementId($check->id()->id() . "-" . $dateCreated->format("Y-m-d") . "-" . $i),
+                $dateCreated,
                 $check,
-                $this->createMeasurementResult()
+                $this->createMeasurementResult($dateCreated)
             );
         }
 
         return $measurements;
     }
 
-    private function createMeasurementResult(): MeasurementResult
+    private function createMeasurementResult(\DateTimeInterface $dateTime): MeasurementResult
     {
-        return new MeasurementResult(
-            200,
-            "test ok",
-            1000,
-            32000
-        );
+        $duration = 200;
+        $memory = 8000;
+        if (6 > $dateTime->format("N")) {
+            // date is NOT weekend
+            $duration += 75 * ((int) $dateTime->format("N"));
+            $memory += 4000 * ((int) $dateTime->format("N"));
+        } elseif (6 == $dateTime->format("N")) {
+            // date is saturday
+            $duration += 75;
+            $memory += 4000;
+        }
+        $time = ((int) $dateTime->format("H")) * 60 + ((int) $dateTime->format("i"));
+        $dayOscillation = sin(M_PI / 2 + 2 * M_PI * $time / (24 * 60));
+        $duration += 23 * $dayOscillation;
+
+        $code = 200;
+        $reasonPhrase = "test ok";
+        if (5 == $dateTime->format("N") && ("11:00" == $dateTime->format("H:i"))) {
+            $code = 500;
+            $reasonPhrase = "server unavailable";
+        }
+        if (1 == $dateTime->format("N") && ("08:00" == $dateTime->format("H:i"))) {
+            $code = 404;
+            $reasonPhrase = "page not found";
+        }
+        if (3 == $dateTime->format("N") && ("17:00" == $dateTime->format("H:i"))) {
+            $code = 0;
+            $reasonPhrase = "something happened!";
+        }
+
+        return new MeasurementResult($code, $reasonPhrase, max(50, $duration), $memory);
     }
 }
