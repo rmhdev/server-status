93ddc39882651ace538df68283c06e7df33db64e
Rober MH <rmh.dev@gmail.com>
2018-05-03 16:21:21 +0200

    user can not access private areas when not authenticated

--- tests/App/Controller/SecurityControllerTest.php
+++ tests/App/Controller/SecurityControllerTest.php
@@ -38,6 +38,79 @@ class SecurityControllerTest extends WebTestCase
         $client->submit($form);
         $client->followRedirect();
 
+
         return $client;
+    }
+
+    /**
+     * @test
+     */
+    public function notAuthenticatedUserShouldHaveLoginLink()
+    {
+        $client = static::createClient();
+        $crawler = $client->request("GET", "/");
+
+        $this->assertEquals(
+            '/login',
+            $crawler->filter('a.sign-in')->attr("href"),
+            'User should have a link to the login form'
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function authenticatedUserShouldHaveLogoutLink()
+    {
+        $client = $this->authenticatedClient();
+        $crawler = $client->getCrawler();
+
+        $this->assertEquals(
+            '/logout',
+            $crawler->filter('a.sign-out')->attr('href'),
+            'Authenticated user should have a link to logout'
+        );
+        $this->assertEquals(
+            0,
+            $crawler->filter('a.sign-in')->count(),
+            'The login link should not appear'
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function accessToAccountByNotAuthenticatedUserShouldRedirectToLogin()
+    {
+        $client = static::createClient();
+        $client->request("GET", "/account");
+
+        $this->assertTrue($client->getResponse()->isRedirection());
+        $client->followRedirect();
+
+        $crawler = $client->request("GET", "/");
+        $this->assertEquals($crawler->getUri(), $client->getHistory()->current()->getUri());
+    }
+
+    /**
+     * @test
+     */
+    public function logoutAuthenticatedUserShouldRedirectToHomepage()
+    {
+        $client = $this->authenticatedClient();
+        $expectedUrl = $client->request("GET", "/")->getUri();
+        $link = $client->getCrawler()
+            ->filter('a.sign-out')
+            ->first()
+            ->link()
+        ;
+        $client->click($link);
+        $client->followRedirect();
+
+        $this->assertEquals(
+            $expectedUrl,
+            $client->getHistory()->current()->getUri(),
+            'Logout redirects to homepage'
+        );
     }
 }
